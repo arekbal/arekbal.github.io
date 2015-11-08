8ca7916ff9e7f56bf172c546f890e68cfca88722
arekbal <arek_bal@wp.pl>
2015-11-08 19:01:41 +0100

    different loaders exposed

--- gherkin_spec/generator.js
+++ gherkin_spec/generator.js
@@ -5,11 +5,31 @@ ko.punches.enableAll();
 
 var that = 
 {
-  filePath: ko.observable("FILE_PATH"),
+  filePath: ko.observable(""),
+  fileAddress: ko.observable(""),
+  loader: ko.observable(""),
+  loaderParam: ko.observable(""),
+  loaderText: ko.pureComputed(function(){
+    if(that.filePath())
+      return "FilePath=\"" + that.filePath() + "\""
+      
+     if(that.fileAddress())
+      return "FileAddress=\"" + that.fileAddress() + "\""
+      
+    if(that.loader())
+    {
+      if(that.loaderParam())
+        return "Loader=typeof(" + that.loader() + "), LoaderParam=\"" + that.loaderParam() + "\""
+      else
+        return "Loader=typeof(" + that.loader() + ")"
+    }
+      
+    return "FilePath=\"\""      
+  }),
   namespaceName: ko.observable("NAMESPACE"),
   inputText: ko.observable(""),
   mode: ko.observable("MsTest"),
-  modes: ko.observableArray(["MsTest"/*, "NUnit", "xUnit"*/]),
+  modes: ko.observableArray(["Base", "MsTest"/*, "NUnit", "xUnit"*/]),
   errorText: ko.observable(""),
   ast: ko.observable(),
   usings: ko.observableArray(),
@@ -18,7 +38,11 @@ var that =
   useKeywords: ko.observable(true),
   knownKeywords: ko.observable("Given, And, But, When, Then"),
   knownKeywordsArray : ko.pureComputed(function(){ return that.knownKeywords().split(' ').join('').split(',') }),
-  allScenariosCoveredTest : ko.observable(true)
+  allScenariosCoveredTest : ko.observable(true),
+  loadExample: function()
+  {
+    that.inputText(exampleFeature);
+  }
 }
 
 that.inputText.extend({ rateLimit: 400 }).subscribe(function(v) 
@@ -64,3 +88,62 @@ function parse(inputText)
 }
 
 parse();
+
+var exampleFeature = (function () {/*
+Feature: Serve coffee
+    Coffee should not be served until paid for
+    Coffee should not be served until the button has been pressed
+    If there is no coffee left then money should be refunded
+
+  Background:
+    Given a global administrator named 'Greg'
+    And a blog named Greg's anti-tax rants
+    And a customer named 'Wilson'
+    And a blog named 'Expensive Therapy' owned by 'Wilson'
+
+  Scenario: Buy last coffee
+    Given there are 1 coffees left in the machine
+    And I have deposited 1$
+	Given the following people exist:
+      | name  | email           | phone |
+      | Aslak | aslak@email.com | 123   |
+      | Joe   | joe@email.com   | 234   |
+      | Bryan | bryan@email.org | 456   |
+    When I press the coffee button
+    Then I should be served a coffee
+	Then the greeting service response will contain one of the following messages:
+      |Hello how are you doing?|
+      |Welcome to the front door!|
+      |How has your day been?|
+      |Come right on in!|
+
+  Scenario: Buy last coffee 2
+    Given there are 1 coffees left in the machine
+    And I have deposited 1$
+	Given the following people exist:
+      | name  | email           | phone |
+      | Aslak | aslak@email.com | 123   |
+      | Joe   | joe@email.com   | 234   |
+      | Bryan | bryan@email.org | 456   |
+    When I press the coffee button
+    Then I should be served a coffee
+	Then the greeting service response will contain one of the following messages:
+      |Hello how are you doing?|
+      |Welcome to the front door!|
+      |How has your day been?|
+      |Come right on in!|
+
+  Scenario Outline: Eating
+    Given there are <start> cucumbers
+    When I eat <eat> cucumbers
+    Then I should have <left> cucumbers
+
+	Examples: First One
+	  | start | eat | left |
+	  |  12   |  5  |  7   |
+	  |  20   |  5  |  15  |
+	Examples: Duda 1
+	  | start | eat | left |
+	  |  12   |  5  |  7   |
+	  |  20   |  5  |  15  |
+*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
