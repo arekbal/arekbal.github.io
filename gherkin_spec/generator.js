d452948c0d49c806aea2373b7d444ea02a32df98
arekbal <arek_bal@wp.pl>
2015-11-08 03:28:35 +0100

    wip over gherkin_spec generator

--- gherkin_spec/generator.js
+++ gherkin_spec/generator.js
@@ -1,13 +1,42 @@
-var input = document.getElementById('input');
-var output = document.getElementById('output');
-var mode = document.getElementById('mode');
-var ns = document.getElementById('ns');
-var parser = new Gherkin.Parser();
-
+var parser = new Gherkin.Parser;
 parser.stopAtFirstError = false;
 
-var template =
-"@foreach"
+var that = 
+{
+  namespaceName: ko.observable(""),
+  inputText: ko.observable(""),
+  mode: ko.observable("MsTest"),
+  errorText: ko.observable(""),
+  usings: ko.observableArray()
+}
+
+that.inputText.extend({ rateLimit: 400 }).subscribe(function(v) 
+{
+  that.errorText(parse(that.inputText()))
+})
+
+that.mode.subscribe(function(v)
+{
+  setMode()
+})
+
+function setMode()
+{
+  var usings = 
+  [
+    "GherkinSpec.Core",
+  ]
+ 
+  if(that.mode() == "MsTest")
+    usings.push("GherkinSpec.MsTest")
+    usings.push("Microsoft.VisualStudio.TestTools.UnitTesting")
+    
+  that.usings(usings)
+}
+
+setMode()
+
+ko.applyBindings(that)
 
 function generate_code(ast)
 {
@@ -39,15 +68,13 @@ function generate_code(ast)
   return usingsText + namespaceBeginText +  + namespaceEndText
 }
 
-function parse() {
-  var result;
+function parse(inputText) {  
   try {
-    var ast = parser.parse(input.value);
-    result = generate_code(ast)
+    var ast = parser.parse(inputText);
+    return generate_code(ast)
   } catch (e) {
-    result = e.stack;
+    return e.message;
   }
-  output.innerHTML = result;
 }
 
 var timeoutHandle = 0
